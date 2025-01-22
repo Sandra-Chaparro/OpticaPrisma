f5a43110cc1f3e28654fd9b0b0815b4149e3035e
sandra chaparro <sandrachaparro@Mac.lan>
2025-01-22 12:43:59 -0600

    finish sliding in services section

--- src/HomePage/ServicesSection/ServicesSection.jsx
+++ src/HomePage/ServicesSection/ServicesSection.jsx
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, { useRef } from "react";
 import "./ServicesSection.css";
 
 export default function ServicesSection() {
+  const scrollContainer = useRef(null);
+
+  const handleScroll = (direccion) =>{
+    if(direccion == "left"){
+      scrollContainer.current.scrollBy({left: -300, behavior: "smooth"})
+    }
+    else{
+      scrollContainer.current.scrollBy({left: 300, behavior: "smooth"})
+    }
+  }
+
+  const services = [
+    {
+      imgSrc: "/Images/eyeExam.jpg",
+      title: "Examen de la Vista"
+    },
+    {
+      imgSrc: "/Images/contacts.jpg",
+      title: "Lentes de Contacto"
+    },
+    {
+      imgSrc: "/Images/lenses.jpg",
+      title: "Lentes graduados"
+    },
+    {
+      imgSrc: "/Images/sunglasses.jpg",
+      title: "Lentes de Sol"
+    },
+    {
+      imgSrc: "/Images/kidsLenses.jpg",
+      title: "Lentes para niños"
+    },
+    {
+      imgSrc: "/Images/accesories.jpg",
+      title: "Accesorios para Lentes"
+    },
+  ]
+
   return (
     <section className="ServicesSectionContainer">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img
-        className='.serviceCardImg'
-          src="/Images/eyeExam.jpg"
-          alt="Examen de la vista en paciente"
-        />
-        <h2>Examen de la vista</h2>
+       <button className="scroll-btn left-btn" onClick={() => handleScroll("left")}>←</button>
+      <div className='gridContainer' ref={scrollContainer}>
+        {services.map((service, index)=>(
+          <div key={index} className='servicesGridCard'>
+            <img
+              src={service.imgSrc}
+              alt={service.title}
+              className='serviceCardImg '
+            />
+            <div className="service-card-text">
+              <h2>
+                {service.title}
+              </h2>
+            </div>
+          </div>
+        ))}
       </div>
-      </section>
+      <button className="scroll-btn right-btn" onClick={() => handleScroll("right")}>→</button>
+    </section>
   )
 }
 
