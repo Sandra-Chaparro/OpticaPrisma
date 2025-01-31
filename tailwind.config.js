d5f6df8130734176441342ce89446c3eaf3d8fd6
sandra chaparro <sandrachaparro@Mac.lan>
2025-01-30 19:31:25 -0600

    script upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        morado: '#887BB0', 
-        azulBajito: '#D4F1F4', 
-        rojo: "#D01110",
-        amarillo: "#FEE900",
-        gris: "#D6D6D6",
-      },
-      screens: {
-        sm: "640px",
-        md: "768px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
