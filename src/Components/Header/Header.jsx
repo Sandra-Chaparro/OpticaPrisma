d08eeeb8b5a268ca1cd3e3afe20b33ca8ceea169
sandra chaparro <sandrachaparro@Mac.lan>
2025-02-05 20:05:11 -0600

    added styles missing to header tabs after modification

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -87,16 +87,22 @@ return (
                   </>
                 )}
               </Disclosure>
-              ) : (              
+              ) : (        
+                                          
                 <NavLink
+                className={classNames(
+                  "block rounded-md px-3 py-2 text-base font-medium ",
+                  item.current
+                    ? "bg-gray-900 text-white"
+                    : "text-white hover:bg-gray-700 hover:text-white"
+                  )}
                   to={item.href}
                   aria-current={({ isActive }) => (isActive ? "page" : undefined)}
                   onClick={() => close()} key={item.name}
                 >
                 {item.name}
                
-                </NavLink>
-
+                </NavLink>      
                 )
               ))}
             </div>
